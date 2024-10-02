ee24a4bd89d76a3e28fcca969de34b71f95c8838
Brunotorres15 <Brunotorres15>
2024-10-02 23:55:09 +0000

    adding initial full ingestion logic

---
+++ src/ingestao.py.py
@@ -0,0 +1,66 @@
+# Databricks notebook source
+# Definir a configuração do parâmetro
+spark.conf.set("spark.sql.legacy.parquet.nanosAsLong", "true")
+
+# COMMAND ----------
+
+catalog = 'bronze'
+database = 'operadoras_full'
+table = 'operadoras'
+
+# COMMAND ----------
+
+def table_exists(catalog, database, table):
+    count = (spark.sql(f"show tables from {catalog}.{database}").filter(f"database = '{database}' and tableName = '{table}'")).count()
+
+    return count == 1
+
+table_exists(catalog, database, table)
+
+
+# COMMAND ----------
+
+df_full = (spark.read.format('parquet')
+    .option("inferSchema", "true")
+    .load('/Volumes/landing/upsell/operadoras/full/'))
+
+df_full.coalesce(1).write.format('delta').saveAsTable(f'{catalog}.{database}.{table}')
+
+# COMMAND ----------
+
+df.display()
+
+# COMMAND ----------
+
+
+
+# COMMAND ----------
+
+# MAGIC %sql
+# MAGIC
+# MAGIC select * from bronze.operadoras_full.operadoras
+# MAGIC
+
+# COMMAND ----------
+
+
+
+# COMMAND ----------
+
+
+
+# COMMAND ----------
+
+
+
+# COMMAND ----------
+
+
+
+# COMMAND ----------
+
+
+
+# COMMAND ----------
+
+
